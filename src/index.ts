14879608a05100e6cefafefd4252ab4356328293
Davoudkhodaverdian <davoudkhodaverdian20@gmail.com>
2023-02-17 17:23:20 +0330

    add nodemon in typescript nodejs project

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
 
 import express from 'express';
+import http from 'http';
+
 const app = express();
 const port = 9000;
+const server = http.createServer(app);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
+
+server.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
 });
+
+
+// app.listen(port, () => {
+//   return console.log(`Express is listening at http://localhost:${port}`);
+// });
